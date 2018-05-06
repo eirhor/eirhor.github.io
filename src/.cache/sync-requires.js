22d0ede36c9451677342ab24484177d01c6ee0e4
Eirik Horvath <eirik@geta.no>
2018-05-06 21:54:12 +0200

    Initial Gatsby setup.

---
+++ src/.cache/sync-requires.js
@@ -0,0 +1,15 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.layouts = {
+
+}
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Projects\\github\\eirhor.github.io\\src\\.cache\\dev-404-page.js"))
+}
+
+exports.json = {
+  "dev-404-page.json": require("C:\\Projects\\github\\eirhor.github.io\\src\\.cache\\json\\dev-404-page.json")
+}
